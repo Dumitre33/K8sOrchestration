81f880791d6b810f15b102758bbbb903fe6d35fd
alex <alexandru.dumitre@gmail.com>
2024-07-04 16:12:25 +0300

    final5

--- k8sconfig/README.md
+++
@@ -1,4 +0,0 @@
-# k8s_app
-
-This is a Quart-based application to manage Kubernetes resources such as Deployments, Services, ConfigMaps, PersistentVolumes, and PersistentVolumeClaims.
-
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='k8sconfig', 
+    name='app', 
     version='0.2',  
     packages=find_packages(), 
     install_requires=requirements,
