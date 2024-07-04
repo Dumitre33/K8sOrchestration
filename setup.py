eb24c35d442cc42201786eec9531174a173436f4
alex <alexandru.dumitre@gmail.com>
2024-07-04 15:11:40 +0300

    final4

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup, find_packages
 
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='k8s_app',
-    version='0.2.0',
-    packages=find_packages(),
-    include_package_data=True,
+    name='k8squart_app', 
+    version='0.2',  
+    packages=find_packages(), 
     install_requires=requirements,
     entry_points={
         'console_scripts': [
             'start_program=thesis.k8s_config.run:main',
         ],
     },
+    author='Alex Dumitre', 
+    author_email='alexandru.dumitre@gmail.com', 
+    description='A python quart poject for Kubernetes management with Micork8s',
+    long_description=open('README.md').read(),
+    long_description_content_type='text',
+    url='git@github.com:Dumitre33/K8sOrchestration.git',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.2',  
 )
-
