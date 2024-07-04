a66d47a4fc88be107584e4168beefefc13ff77b4
alex <alexandru.dumitre@gmail.com>
2024-07-04 15:41:53 +0300

    final4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='k8squart_app', 
+    name='thesis', 
     version='0.2',  
     packages=find_packages(), 
     install_requires=requirements,
