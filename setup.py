c0e0af7d4767d650884cfe51331b3a5bfaae8be4
alex <alexandru.dumitre@gmail.com>
2024-07-08 15:08:51 +0300

    finall

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-# Function to read the requirements.txt file
 def parse_requirements(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -25,14 +24,6 @@ setup(
 
     author='Alex Dumitre', 
     author_email='alexandru.dumitre@gmail.com', 
-    description='A python quart poject for Kubernetes management with Micork8s',
-    long_description=open('README.md').read(),
-    long_description_content_type='text',
     url='https://github.com/Dumitre33/K8sOrchestration/tree/master/k8sconfig',  
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.2',  
+    
 )
